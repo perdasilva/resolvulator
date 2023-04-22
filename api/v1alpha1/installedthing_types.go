91536077d91cf45e121babb5d43a6d419421c1c2
perdasilva <perdasilva@redhat.com>
2023-04-22 11:04:41 +0200

    Add InstalledThing resource
    Signed-off-by: perdasilva <perdasilva@redhat.com>

---
+++ api/v1alpha1/installedthing_types.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// InstalledThingSpec defines the desired state of InstalledThing
+type InstalledThingSpec struct {
+}
+
+// InstalledThingStatus defines the observed state of InstalledThing
+type InstalledThingStatus struct {
+	Phase              string             `json:"phase,omitempty"`
+	ObservedGeneration int64              `json:"observedGeneration,omitempty"`
+	Conditions         []metav1.Condition `json:"conditions,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:resource:scope=Cluster
+//+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name=Phase,type=string,JSONPath=`.status.phase`
+//+kubebuilder:printcolumn:name=Age,type=date,JSONPath=`.metadata.creationTimestamp`
+
+// InstalledThing is the Schema for the installedthings API
+type InstalledThing struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   InstalledThingSpec   `json:"spec,omitempty"`
+	Status InstalledThingStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// InstalledThingList contains a list of InstalledThing
+type InstalledThingList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []InstalledThing `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&InstalledThing{}, &InstalledThingList{})
+}
--- main.go
+++ main.go
@@ -95,6 +95,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Thing")
 		os.Exit(1)
 	}
+	if err = (&controllers.InstalledThingReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "InstalledThing")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
